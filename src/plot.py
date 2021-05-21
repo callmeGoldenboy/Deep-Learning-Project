edeebd0f1cfab6b4b3f4940e68d1c467395c803e
google-cloud <djiar@kth.se>
2021-05-21 12:07:30 +0000

    plot fix

--- src/plot.py
+++ src/plot.py
@@ -24,27 +24,35 @@ def plot_loss_and_accuracy(cnn,name="figure"):
     train_loss = cnn.history["loss"]
     val_acc = cnn.history["val_accuracy"]
     val_loss = cnn.history["val_loss"]
+    n = len(cnn.history["accuracy"])
     x = list(range(1,len(cnn.history["accuracy"]) + 1))
-    xticks = list(range(1,int(len(cnn.history["accuracy"])) + 1,int(len(cnn.history["accuracy"]) / 5)))
+    xticks = range(1,n + 1, int(round(n / 9)) + 1)
+    yticks_acc = [round(i*0.1,1) for i in range(0,11)]
+    yticks_loss = [round(i*0.5,1) for i in range(0,9)]
     print(x)
-    fig,(ax1,ax2) = plt.subplots(1,2)
+    fig,(ax1,ax2) = plt.subplots(2,1)
+    #ax1.set_autoscale_on(False)
     ax1.plot(x,train_acc,'r')
     ax1.plot(x,val_acc,'g')
     ax1.set_xticks(xticks)
-    ax1.set_xlabel("Num of Epochs")
+    ax1.set_yticks(yticks_acc)
+    #ax1.set_xlabel("Num of Epochs")
     ax1.set_ylabel("Accuracy")
-    ax1.set_title("Accuracy")
+    #ax1.set_title("Accuracy")
     ax1.legend(['train','validation'])
     #loss plots 
+    
+    #ax2.set_autoscale_on(False)
     ax2.plot(x,train_loss,'r')
     ax2.plot(x,val_loss,'g')
     ax2.set_xticks(xticks)
+    ax2.set_yticks(yticks_loss)
     ax2.set_xlabel("Num of Epochs")
     ax2.set_ylabel("Loss")
-    ax2.set_title("Loss")
+    #ax2.set_title("Loss")
     ax2.legend(['train','validation'])
     fig.subplots_adjust(wspace=0.5)
-    #fig.suptitle("Accuracy and Loss for Training and Validation")
+    fig.suptitle("Accuracy and Loss")
     
    
     plt.savefig(name)
